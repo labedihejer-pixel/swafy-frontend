6c5d53d61d2d2912eb3bb33d447044adfa6ed6ac
labedihejer-pixel <labedihejer@gmail.com>
2026-05-02 11:04:42 +0100

    temp rename login to force case change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Login from "./pages/Login";
+import Login from "./pages/tempp";
 import Register from "./pages/Register";
 import AdminDashboard from "./pages/AdminDashboard";
 import AdminLiveStream from "./pages/AdminLiveStream";
